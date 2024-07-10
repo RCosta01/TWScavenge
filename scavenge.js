66ddb9594f794e8d4e186402e955bc5ecf668380
RCosta01 <127419203+RCosta01@users.noreply.github.com>
2024-07-10 22:14:38 +0100

    Update scavenge.js

--- scavenge.js
+++ scavenge.js
@@ -144,7 +144,7 @@ function scavenge() {
         }
         for (i = 0; i < $(".border-frame-gold-red").length; i++) {
             cat = document.createElement('div');
-            cat.innerHTML = '<div align="center"><h3>Enable</h3><input type="checkbox" ID="haul' + (i + 1) + 'Enabled" checked="' + haulValueCheckboxes[i] + '" ><hr></div>';
+            cat.innerHTML = '<div align="center"><h3>Enable</h3><input type="checkbox" ID="haul' + (i + 1) + 'Enabled" checked=' + haulValueCheckboxes[i] + ' ><hr></div>';
             $(".border-frame-gold-red")[i].prepend(cat);
         }
 
